968397e98929c6b65b94800e682b9c30b554bb63
Rivgad <losvermo@gmail.com>
2021-10-29 20:47:49 +0300

    Вывод отелей

--- TravelAgency/TravelAgency/HotelsTable.xaml.cs
+++ TravelAgency/TravelAgency/HotelsTable.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,22 @@ namespace TravelAgency
     /// </summary>
     public partial class HotelsTable : Page
     {
+        private class Hotel
+        {
+            public string Title { get; set; }
+            public string Stars { get; set; }
+            public string Country { get; set; }
+            public string ToursCount { get; set; }
+        }
+        private ObservableCollection<Hotel> _hotels = new ObservableCollection<Hotel>();
+
         public HotelsTable()
         {
             InitializeComponent();
+            HotelsDataGrid.ItemsSource = _hotels;
+
+
+            _hotels.Add(new Hotel() { Title = "Гостиница Татарстан", Stars = "4", Country = "Россия", ToursCount = "10" });
         }
     }
 }
