c97ab3de30b3aa0bd4ce19328036e9cf47bb4431
Rivgad <losvermo@gmail.com>
2021-10-31 21:07:29 +0300

    Фильтрация туров и изменени стилей Редактировал стили Сделал фильтрацию туров

--- TravelAgency/TravelAgency/Pages/Tours/TourListPage.xaml.cs
+++ TravelAgency/TravelAgency/Pages/Tours/TourListPage.xaml.cs
@@ -20,12 +20,53 @@ namespace TravelAgency
     /// </summary>
     public partial class TourList : Page
     {
-        
+
         public TourList()
         {
             InitializeComponent();
+
+            var allTypes = RussiaTravelEntities.Context.Type.ToList();
+            allTypes.Insert(0, new Type { Name = "Все типы" });
+            CBoxType.ItemsSource = allTypes;
+            CBoxType.SelectedIndex = 0;
+
+            ChBoxActual.IsChecked = true;
+
+            UpdateTours();
+        }
+
+        private void UpdateTours()
+        {
             var currentTours = RussiaTravelEntities.Context.Tour.ToList();
-            LViewTours.ItemsSource = currentTours;
+            
+            if(CBoxType.SelectedIndex != 0)
+            {
+                currentTours = currentTours.Where(tour => tour.Type.Contains(CBoxType.SelectedItem as Type)).ToList();
+            }
+
+            currentTours = currentTours.Where(tour => tour.Name.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            if(ChBoxActual.IsChecked.Value == true)
+            {
+                currentTours = currentTours.Where(tour => tour.IsActual == true).ToList();
+            }
+
+            LViewTours.ItemsSource = currentTours.OrderBy(tour => tour.TicketCount).ToList();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateTours();
+        }
+
+        private void CBoxType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateTours();
+        }
+
+        private void ChBoxActual_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateTours();
         }
     }
 }
